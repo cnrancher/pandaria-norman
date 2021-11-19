03e5bc88bbd2fdfd044df7ae81a3070e69edc047
niusmallnan <niusmallnan@gmail.com>
2021-11-19 17:34:33 +0800

    PANDARIA: [backport] added support for json lines encoding

--- parse/parse.go
+++ parse/parse.go
@@ -21,9 +21,10 @@ const (
 var (
 	multiSlashRegexp = regexp.MustCompile("//+")
 	allowedFormats   = map[string]bool{
-		"html": true,
-		"json": true,
-		"yaml": true,
+		"html":  true,
+		"json":  true,
+		"jsonl": true,
+		"yaml":  true,
 	}
 )
 
@@ -270,11 +271,20 @@ func parseResponseFormat(req *http.Request) string {
 	if isYaml(req) {
 		return "yaml"
 	}
+
+	if isJSONL(req) {
+		return "jsonl"
+	}
+
 	return "json"
 }
 
 func isYaml(req *http.Request) bool {
 	return strings.Contains(req.Header.Get("Accept"), "application/yaml")
+}
+
+func isJSONL(req *http.Request) bool {
+	return strings.Contains(req.Header.Get("Accept"), "application/jsonl")
 }
 
 func parseMethod(req *http.Request) string {
